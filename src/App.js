d9917a0466ff6c228c711545e7b2240a9e308417
Grigor Tamazyan <grigtm@gmail.com>
2020-06-20 00:11:19 +0400

    third commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/Navbar/Navbar'
+import './App.scss';
+import TourList from './components/TourList'
+function App() {
+  return (
+  <div>
+    <Navbar/>
+    <TourList/>
+  </div>);
+  
+}
+
+export default App;
